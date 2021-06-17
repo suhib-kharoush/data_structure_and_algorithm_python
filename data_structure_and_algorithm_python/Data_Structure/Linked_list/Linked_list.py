5c06c26eb22c1693a9fc925d9768e8415773b2e1
suhib-kharoush <suhaib_kharwash@outlook.com>
2021-06-17 18:59:45 +0300

    ll_insertion_finished

--- data_structure_and_algorithm_python/Data_Structure/Linked_list/Linked_list.py
+++ data_structure_and_algorithm_python/Data_Structure/Linked_list/Linked_list.py
@@ -80,53 +80,9 @@ class LinkedList:
                    current.next = new_node
                 current = current.next
 
-    def kthFromEnd(self, k, from_node=None):
-        current = self.head
-        if current == None:
-            return print("linked list is empty")
-        if k < 0:
-            return print("k should be a larger or equal to 0")
-
-        length = 0
-        while current is not None:
-            current = current.next
-            length += 1
-        if k > length:
-             print("kth is greater than the linked-list")
-             return
-
-        current = self.head
-        if from_node:
-            for i in range(from_node, length - k - 2):
-                current = current.next
-            print(current.value)
-        else:
-            for i in range(0, length - k - 1):
-                current = current.next
-            print(current.value)
-  
-  
-    def empty_ll(self):
-        self.head = None
-        return
+ 
 
 
-    # def zipLists(list_one, list_two):
-    #     if not list_one:
-    #         return list_two
-    #     elif not list_two:
-    #         return list_one
-    #     zip_list = LinkedList()
-    #     current = list_one
-    #     current = current.head
-    #     current_2 = list_two.head
-
-    #     while current or current_2:
-    #         if current:
-    #             zip_list.append(current.data)
-    #             current = current.next_node
-    #         return zip_list
-            
 
 
 if __name__ == "__main__":
