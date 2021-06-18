deef242e99b56a3c416e2d31a1525cbd36f04b74
suhib-kharoush <suhaib_kharwash@outlook.com>
2021-06-18 17:59:31 +0300

    edit

--- data_structure_and_algorithm_python/Data_Structure/Linked_list/Linked_list.py
+++ data_structure_and_algorithm_python/Data_Structure/Linked_list/Linked_list.py
@@ -111,23 +111,6 @@ class LinkedList:
         return
 
 
-    # def zipLists(list_one, list_two):
-    #     if not list_one:
-    #         return list_two
-    #     elif not list_two:
-    #         return list_one
-    #     zip_list = LinkedList()
-    #     current = list_one
-    #     current = current.head
-    #     current_2 = list_two.head
-
-    #     while current or current_2:
-    #         if current:
-    #             zip_list.append(current.data)
-    #             current = current.next_node
-    #         return zip_list
-            
-
 
 if __name__ == "__main__":
     ll1 = LinkedList()
