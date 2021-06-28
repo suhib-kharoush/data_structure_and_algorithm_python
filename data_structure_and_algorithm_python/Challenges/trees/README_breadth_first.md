bfd5bb9a45286889347777a9074809e1028b4be0
suhib-kharoush <suhaib_kharwash@outlook.com>
2021-06-28 21:57:07 +0300

    finish

---
+++ data_structure_and_algorithm_python/Challenges/trees/README_breadth_first.md
@@ -0,0 +1,12 @@
+# Breadth first.
+
+### Challenge:
+- Create a breadth first traversal method which takes a binary tree as a unique input.
+
+
+### Whiteboard
+![](breadth_first_tree.PNG)
+
+### Approach & Efficiency:
+- Time: O(n)
+- Space: O(n)
--- tests/Challenges/test_trees.py
+++ tests/Challenges/test_trees.py
@@ -44,6 +44,20 @@ def test_find_max_value_within_tree(tree_test):
     actual=tree_test.find_maximum_value()
     assert actual == expected 
 
+def test_breadth_first():
+    input = BinaryTree()
+    input.root = Node(2)
+    input.root.left = Node(7)
+    input.root.right = Node(5)
+    input.root.left.right = Node(6)
+    input.root.left.left = Node(2)
+    input.root.right.right = Node(9)
+    input.root.right.right.left = Node(4)
+    input.root.left.left.right = Node(5)
+    input.root.left.right.left = Node(11)
+    expected = [2, 7, 5, 2, 6, 9, 5, 11, 4]
+    actual = breadth_first(input)
+    assert actual == expected
 
 @pytest.fixture
 def tree_test():
